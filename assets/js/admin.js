8ee0dc2bc372cd19e99469b6e1ea54874a9c2d0a
Samuel Challe <samuel.challe@gmail.com>
2022-05-09 22:49:14 +0200

    wip

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {OpenApiAdmin, ResourceGuesser} from '@api-platform/admin';
 
+import {SchoolCreate, SchoolList, SchoolEdit, SchoolShow, SchoolIcon} from './schools';
+
 const Admin = () =>
     <OpenApiAdmin docEntrypoint="http://localhost/api/docs.json" entrypoint="http://localhost/api">
         <ResourceGuesser name={"job_descriptions"} />
         <ResourceGuesser name={"job_titles"} />
         <ResourceGuesser name={"pictures"} />
         <ResourceGuesser name={"ressources"} />
-        <ResourceGuesser name={"schools"} />
+        <ResourceGuesser name={"schools"} list={SchoolList} edit={SchoolEdit} create={SchoolCreate} show={SchoolShow} icon={SchoolIcon}/>
         <ResourceGuesser name={"skills"} />
     </OpenApiAdmin>;
 
